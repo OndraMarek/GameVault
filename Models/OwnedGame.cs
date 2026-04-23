05950902e74d057b54590bc11abe7ecaa27165ea
OndraMarek <czondrejmarek@gmail.com>
2026-04-23 22:10:37 +0200

    Add project files.

---
+++ Models/OwnedGame.cs
@@ -0,0 +1,21 @@
+﻿namespace GameVault.Models;
+
+public enum GamingPlatform
+{
+    Steam,
+    EpicGames,
+    PlayStation,
+    Xbox,
+    GOG,
+    NintendoSwitch,
+    UbisoftConnect,
+    Other
+}
+
+public record OwnedGame(
+    Guid Id,
+    int IgdbId,
+    string Title,
+    GamingPlatform Platform,
+    int PlaytimeHours
+);
---
+++ Program.cs
@@ -0,0 +1,19 @@
+using GameVault.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<OwnedGame> myGames = [
+    new OwnedGame(Guid.NewGuid(), 1942, "The Witcher 3: Wild Hunt", GamingPlatform.GOG, 120),
+    new OwnedGame(Guid.NewGuid(), 732, "Grand Theft Auto V", GamingPlatform.Steam, 300),
+    new OwnedGame(Guid.NewGuid(), 731, "Grand Theft Auto IV", GamingPlatform.Steam, 200),
+    new OwnedGame(Guid.NewGuid(), 113, "Fortnite", GamingPlatform.EpicGames, 45),
+];
+
+app.MapGet("/api/mygames", () => myGames);
+
+app.MapGet("/api/mygames/steam", () => myGames.Where(myGame => myGame.Platform == GamingPlatform.Steam));
+
+app.MapGet("/api/mygames/{id}", (Guid id) => myGames.FirstOrDefault(myGame => myGame.Id == id));
+
+app.Run();
